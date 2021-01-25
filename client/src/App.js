2d66082328e5cdb0e21e6c90d276db5f3c30e607
Michael Venturi <michaelventuri58@aol.com>
2021-01-25 11:41:40 -0500

    setup basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+import Product from "./pages/Product";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/products/:productId" component={Product} />
+    </Router>
+  );
 }
 
 export default App;
